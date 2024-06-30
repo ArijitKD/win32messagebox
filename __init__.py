7f573cb463e0b3668ff4cd2fb3d84908470d65df
Arijit Kumar Das <arijitkrdas2004@outlook.com>
2024-07-01 00:36:16 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -22,10 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-
-
 from os import name
-
 if (name == 'nt'):
     from tkinter import messagebox
 else:
